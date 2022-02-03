3ca5cf6f755b4ae3d5fe83d194fb61bb451077f0
Jose Vega <jocvegar@gmail.com>
2022-02-03 08:30:02 -0600

    add geolocation

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,16 +1,48 @@
 <script setup>
-import { ref } from "vue";
-
+import { ref, onMounted } from "vue";
 defineProps({
   msg: String,
 });
-
 const count = ref(0);
+const location = ref(null);
+const gettingLocation = ref(false);
+const errorStr = ref(null);
+
+onMounted(() => {
+  getStuff();
+});
+
+function getStuff() {
+  if (!("geolocation" in navigator)) {
+    errorStr.value = "Geolocation is not available.";
+    return;
+  }
+  gettingLocation.value = true;
+  navigator.geolocation.getCurrentPosition(
+    (position) => {
+      location.value = position.coords;
+      gettingLocation.value = false;
+    },
+    (error) => {
+      errorStr.value = error.message;
+      gettingLocation.value = false;
+    }
+  );
+}
 </script>
 
 <template>
   <h1>{{ msg }}</h1>
-
+  <div v-if="errorStr">
+    Sorry, but the following error occurred: {{ errorStr }}
+  </div>
+  <div v-if="gettingLocation">
+    <i>Getting your location...</i>
+  </div>
+  <div v-if="location">
+    Your location data is {{ location.coords.latitude }},
+    {{ location.coords.longitude }}
+  </div>
   <p>
     Recommended IDE setup:
     <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
@@ -18,7 +50,6 @@ const count = ref(0);
     <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
   </p>
   <p>TIME: {{ new Date() }}</p>
-
   <p>
     <a href="https://vitejs.dev/guide/features.html" target="_blank">
       Vite Documentation
@@ -28,10 +59,6 @@ const count = ref(0);
   </p>
 
   <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
 </template>
 
 <style scoped>
